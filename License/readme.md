36752cd3218c45681a7808afdc13bbd9dc5740a3
lhp9916 <lhp9916@gmail.com>
2017-04-02 11:28:38 +0800

    delete all

--- License/readme.md
+++
@@ -1,6 +0,0 @@
-## 如何选择开源许可证？
-
-最流行的六种：
-GPL、BSD、MIT、Mozilla、Apache 和 LGPL。
-
-![](../images/license.png)
--- gulp-imagemin/gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const imagemin = require('gulp-imagemin');
-
-gulp.task('compress', () =>
-    gulp.src('src/images/*/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('dist/images'))
-);
